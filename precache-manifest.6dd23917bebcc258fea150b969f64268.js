7b18ce33a447073cddfb64e6b21fb3d2422086ea
amal.mohamed <amal.mohamed@bluecrunch.com>
2020-07-09 01:00:40 +0200

    Updates

---
+++ precache-manifest.6dd23917bebcc258fea150b969f64268.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adba32c1aa0fd90352f182085c2e3e9b",
+    "url": "/react-blog/index.html"
+  },
+  {
+    "revision": "d7fbb8632510796d2492",
+    "url": "/react-blog/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "48ce2b5d0b48f7d3a20a",
+    "url": "/react-blog/static/css/main.75a7873d.chunk.css"
+  },
+  {
+    "revision": "d7fbb8632510796d2492",
+    "url": "/react-blog/static/js/2.9e829fec.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/react-blog/static/js/2.9e829fec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48ce2b5d0b48f7d3a20a",
+    "url": "/react-blog/static/js/main.8855b440.chunk.js"
+  },
+  {
+    "revision": "0bca87922476c71d01b6",
+    "url": "/react-blog/static/js/runtime-main.796b35e1.js"
+  },
+  {
+    "revision": "53403f3118fad4f802bfbe9433198ca5",
+    "url": "/react-blog/static/media/bg_1.53403f31.jpg"
+  }
+]);
